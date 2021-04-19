652a10c251e61df9ea35c2fd3b46a06f313068e6
Martingoesforword <song8713@qq.com>
2021-04-20 00:24:44 +0800

    vba提交

---
+++ excel/song.vb
@@ -0,0 +1,14 @@
+Sub func(input)
+'
+' description.
+'
+' @since 1.0.0
+' @param {type} [name] description.
+' @return {type} [name] description.
+' @see dependencies
+'
+
+    Dim a = .Range(.Cells(1), .Cells(1))
+    
+
+End Sub
